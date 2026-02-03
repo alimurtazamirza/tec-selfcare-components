feedaf060c4b28f1a2b0a2410e1c87aaf7d66344
Ali murtaza <ali.murtaza@evampsaanga.com>
2026-02-03 15:17:42 +0500

    index import change

--- src/index.js
+++ src/index.js
@@ -1,12 +1,6 @@
 // Export all components from a single entry point
-export { default as LoginPage } from "./LoginPage/index.js";
-export { default as DashboardPage } from "./DashboardPage/index.js";
-
-// You can also create named exports for convenience
 import LoginPage from "./LoginPage/index.jsx";
 import DashboardPage from "./DashboardPage/index.jsx";
 
-export default {
-  LoginPage,
-  DashboardPage,
-};
+// Export all components as named exports
+export { LoginPage, DashboardPage };
