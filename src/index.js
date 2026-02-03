0ac28a8314483645a3863c63546f9b2ecfb87908
Ali murtaza <ali.murtaza@evampsaanga.com>
2026-02-03 14:48:10 +0500

    jsx syntac

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ export { default as LoginPage } from "./LoginPage/index.js";
 export { default as DashboardPage } from "./DashboardPage/index.js";
 
 // You can also create named exports for convenience
-import LoginPage from "./LoginPage/index.js";
-import DashboardPage from "./DashboardPage/index.js";
+import LoginPage from "./LoginPage/index.jsx";
+import DashboardPage from "./DashboardPage/index.jsx";
 
 export default {
   LoginPage,
